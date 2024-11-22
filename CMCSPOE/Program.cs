7d191ae33b8e7556c3c895425dfb81dbdc07f7a1
ST10366285 <ST10366285@vcconnect.edu.za>
2024-11-22 22:41:02 +0200

    Commit 4- Program.cs 2

--- CMCSPOE/Program.cs
+++ CMCSPOE/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("CMMCnn")));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DataSystemCnn")));
 
 var app = builder.Build();
 
