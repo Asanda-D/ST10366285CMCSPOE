d0f4dcd7ed5d9a4d27ca1fd9169168f0e1140762
ST10366285 <ST10366285@vcconnect.edu.za>
2024-11-22 22:37:02 +0200

    Commit 3 - Program.cs

--- CMCSPOE/Program.cs
+++ CMCSPOE/Program.cs
@@ -4,15 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adding services to container
+// Add services to the container.
 
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+   .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("DataSystemCnn")));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("CMMCnn")));
 
 var app = builder.Build();
 
@@ -39,5 +42,118 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager =
+        scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Manager ", "Coordinator", "HR" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    // Create and assign role to the "Coordinator" user
+    string email = "asandad@coordinator.com";
+    string password = "VCWestville2005!";
+
+    var user = await userManager.FindByEmailAsync(email);
+    if (user == null)
+    {
+        user = new IdentityUser { UserName = email, Email = email };
+
+        // Create the user
+        var createUserResult = await userManager.CreateAsync(user, password);
+
+        if (createUserResult.Succeeded)
+        {
+            // If user creation is successful, add the user to the "Coordinator" role
+            await userManager.AddToRoleAsync(user, "Coordinator");
+        }
+        else
+        {
+            // Handle user creation failure (optional: log the errors)
+            foreach (var error in createUserResult.Errors)
+            {
+                Console.WriteLine($"Error creating user: {error.Description}");
+            }
+        }
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    // Create and assign role to the "Manager" user
+    string email = "asandad@manager.com";
+    string password = "Clifton2005!";
+
+    var user = await userManager.FindByEmailAsync(email);
+    if (user == null)
+    {
+        user = new IdentityUser { UserName = email, Email = email };
+
+        // Create the user
+        var createUserResult = await userManager.CreateAsync(user, password);
+
+        if (createUserResult.Succeeded)
+        {
+            // If user creation is successful, add the user to the "Manager" role
+            await userManager.AddToRoleAsync(user, "Manager");
+        }
+        else
+        {
+            // Handle user creation failure (optional: log the errors)
+            foreach (var error in createUserResult.Errors)
+            {
+                Console.WriteLine($"Error creating user: {error.Description}");
+            }
+        }
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    // Create and assign role to the "HR" user
+    string email = "asandad@HR.com";
+    string password = "2005Clifton!";
+
+    var user = await userManager.FindByEmailAsync(email);
+    if (user == null)
+    {
+        user = new IdentityUser { UserName = email, Email = email };
+
+        // Create the user
+        var createUserResult = await userManager.CreateAsync(user, password);
+
+        if (createUserResult.Succeeded)
+        {
+            // If user creation is successful, add the user to the "HR" role
+            await userManager.AddToRoleAsync(user, "HR");
+        }
+        else
+        {
+            // Handle user creation failure (optional: log the errors)
+            foreach (var error in createUserResult.Errors)
+            {
+                Console.WriteLine($"Error creating user: {error.Description}");
+            }
+        }
+    }
+}
+
+
 
 app.Run();
